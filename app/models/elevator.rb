71f42f5b80a02af84c2da59c73b8225168e91df9
alberbecois <joshua.knutson@outlook.com>
2020-05-14 12:46:54 -0400

    turn off sms

--- app/models/elevator.rb
+++ app/models/elevator.rb
@@ -3,7 +3,7 @@ require 'Send_SMS/SMS'
 class Elevator < ApplicationRecord
     belongs_to :column
 
-    after_save :send_sms
+    # after_save :send_sms
 
     def send_sms
         if (self.saved_change_to_status? && self.status == "intervention")
